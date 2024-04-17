910c52943a587653af364da853cd47a3e826c5a7
Justin Robinson <justin.robinson@vcontractor.co.za>
2024-04-17 18:52:24 +0200

    allow for dev mode usage

--- src/main/java/com/fluidnotions/jwttokenparameterannotation/JwtTokenArgumentResolver.java
+++ src/main/java/com/fluidnotions/jwttokenparameterannotation/JwtTokenArgumentResolver.java
@@ -11,8 +11,11 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.RecordComponent;
+import java.util.Arrays;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class JwtTokenArgumentResolver implements HandlerMethodArgumentResolver {
@@ -25,19 +28,17 @@ public class JwtTokenArgumentResolver implements HandlerMethodArgumentResolver {
     }
 
     @Override
-    public Optional<Object> resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
         String token = webRequest.getHeader("Authorization");
         log.debug("token: {}", token);
+        Class<?> tokenClass = parameter.getParameterAnnotation(JwtTokenHeader.class).value();
         if(token != null){
             var tokenRaw = token.substring(7);
             log.debug("tokenRaw: {}", tokenRaw);
             Map<String, Object> decodedToken = decodeToken(tokenRaw);
-            Class<?> tokenClass = parameter.getParameterAnnotation(JwtTokenHeader.class).value();
             return convertTokenToObject(decodedToken, tokenClass);
-        }else{
-            log.warn("Token is null, direct call to endpoint without token, likely not via the gateway and using dev mode");
         }
-        return Optional.empty();
+        return getEmptyClassInstance(tokenClass);
     }
 
     private Map<String, Object> decodeToken(String token) {
@@ -45,8 +46,31 @@ public class JwtTokenArgumentResolver implements HandlerMethodArgumentResolver {
         return jwt.getClaims().entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().as(Object.class)));
     }
 
-    private Optional<Object> convertTokenToObject(Map<String, Object> token, Class<?> tokenClass) {
-        var clazz = new ObjectMapper().convertValue(token, tokenClass);
-        return Optional.of(clazz);
+    private Object convertTokenToObject(Map<String, Object> token, Class<?> tokenClass) {
+        return new ObjectMapper().convertValue(token, tokenClass);
+    }
+
+    private Object getEmptyClassInstance(Class<?> tokenClass) {
+        Object instance = null;
+        if (tokenClass.isRecord()) {
+            try {
+                RecordComponent[] components = tokenClass.getRecordComponents();
+                Object[] nulls = new Object[components.length];
+                Constructor<?> constructor = tokenClass.getConstructor(Arrays.stream(components).map(RecordComponent::getType).toArray(Class[]::new));
+                instance = constructor.newInstance(nulls);
+            } catch (NoSuchMethodException | InstantiationException | IllegalAccessException |
+                     InvocationTargetException e) {
+                log.error("Error creating empty instance of record class", e);
+            }
+        }
+        else {
+            try {
+                instance = tokenClass.getDeclaredConstructor().newInstance();
+            } catch (InstantiationException | IllegalAccessException | NoSuchMethodException |
+                     InvocationTargetException e) {
+                log.error("Error creating empty instance of class", e);
+            }
+        }
+        return instance;
     }
 }
